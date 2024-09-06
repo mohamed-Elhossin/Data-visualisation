389313ba61c44accba8bfd1fd062544df1033985
mohamed-Elhossin <m.m.m.elhossin@gmail.com>
2024-09-06 19:07:34 +0300

    final

--- sketch.js
+++ sketch.js
@@ -1,6 +1,17 @@
 
 // Global variable to store the gallery object. The gallery object is
 // a container for all the visualisations.
+
+
+let chartweather = require('./asset/weather');
+let covid = require('./asset/covid');
+let DailyTemperatur = require('./asset/DailyTemperatur');
+// let PieChart = require('./asset/PieChart');
+// let sales = require('./asset/sales');
+let tree = require('./asset/tree');
+let weather = require('./asset/weather');
+
+
 var gallery;
 
 function setup() {
@@ -19,6 +30,14 @@ function setup() {
   gallery.addVisual(new PayGapTimeSeries());
   gallery.addVisual(new ClimateChange());
 }
+gallery.addVisual(new chartweather());
+gallery.addVisual(new covid());
+gallery.addVisual(new DailyTemperatur());
+gallery.addVisual(new PieChart());
+gallery.addVisual( new sales());
+gallery.addVisual(new tree());
+gallery.addVisual(new weather());
+
 
 function draw() {
   background(255);
